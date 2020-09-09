196569259be7934212cbfb444e97060c58ea1213
irockmywarlock <noemirsevilla@gmail.com>
2020-09-09 18:27:14 -0500

    first init

---
+++ hello.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return render_template('index.html', phrase='hello', times=5)  # Return the string 'Hello World!' as a response
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)   
+@app.route('/success')
+def success():
+  return "success"
+@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def hello(name):
+    print(name)
+    return "Hello, " + name
+@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def hello(name):
+    print(name)
+    return "Hello, " + name
+  
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+       {'name' : 'Michael', 'age' : 35},
+       {'name' : 'John', 'age' : 30 },
+       {'name' : 'Mark', 'age' : 25},
+       {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
